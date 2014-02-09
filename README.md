807c1362497b021468d95c32471fe3c252adac6f
Gregor Martynus <gregor@martynus.net>
2014-02-09 22:02:29 +0100

    HTML5 is dead. Gave up – @horse_js

---
+++ README.md
@@ -0,0 +1,84 @@
+appCache AutoUpdate
+===================
+
+> Auto Updates for Offline First Applications apps
+
+Installation
+------------
+
+Install using [bower](http://bower.io/) for usage in browser:
+
+```
+bower install --save appcache-autoupdate
+```
+
+Usage
+-----
+
+Make sure the `manifest` attribute is set on the `<html>` tag.
+
+```html
+<html manifest="appcache.manifest">
+<script src="appache-autoupdate.js">
+```
+
+AutoUpdate starts checking automatically for updates every
+30 seconds. To prevent that, add `data-autoupdate="false"`.
+You can also alter the check interval by setting a time
+in ms: `data-autoupdate="10000"`.
+
+The JS API:
+
+```js
+AutoUpdate.on('updateready', reloadPage)
+AutoUpdate.on('error', handleError)
+
+// AutoUpdate also offers reliable online/offline events
+// in case the cache manifest could not be downloaded
+AutoUpdate.on('offline', showOfflineIndicator)
+AutoUpdate.on('online', hideOfflineIndicator)
+
+// set interval for checking for updates in ms
+// (default is 30000)
+AutoUpdate.setInterval(60000)
+
+// set alternative interval when offline
+AutoUpdate.setOfflineInterval(30000)
+
+// reload page when routing, if update is ready
+function onRoute(event) {
+  if (AutoUpdate.hasUpdate()) {
+    location.reload(1)
+  }
+}
+```
+
+Use Case
+--------
+
+I extracted `appCache-autoupdate.js` out of minutes.io, which is an [Auto Updates for Offline First Applications](http://Auto Updates for Offline First Applications.org/)
+web application, anno 2011. It's battle by a ton of users, devices, internet environment.
+
+minutes.io checks every 30 seconds if an update is available. And whenever the user navigates
+from one view to another, it reloads the page in case there is. As the assets are all cached,
+the user cannot tell that his page got just reloaded. It's silent, without any notification,
+or a prompt asking the user to reload the page. And it works very well so far.
+
+Demo
+----
+
+I don't have a demo app ready yet, I'm happily accepting pull requests though.
+But here's my idea for a tiny node.js app, hosted for free on Heroku or whereever:
+
+1. Autogenerate `appcache.manifest` that changes every 30s
+2. Autogenerate `theme.css` that changes the background color every time it gets requested
+3. Maka a tiny application with some tabs that the user can navigate between. Make sure the
+   URL changes so the app can be reloaded & remain at the current tab.
+
+Fine Print
+----------
+
+appCache Autoupdate has been authored by [Gregor Martynus](https://github.com/gr2m),
+proud member of [Team Hoodie](http://hood.ie/). Support our work: [gittip us](https://www.gittip.com/hoodiehq/).
+
+License: MIT
---
+++ appcache-autoupdate.js
@@ -0,0 +1,173 @@
+(function(global){
+  'use strict';
+
+  // AutoUpdate
+  // ==========
+  //
+  // a helper class for autoupdating HTML5 offline cache (appcache)
+  // Recommended reads:
+  //
+  // - http://www.html5rocks.com/en/tutorials/appcache/beginner/
+  // - http://alistapart.com/article/application-cache-is-a-douchebag
+  //
+  var AutoUpdate = (function() { // public event API, requires jQuery/Zepto
+    var fallback = { bind: noop, on: noop, trigger: noop, unbind: noop };
+    var eventProvider = global.jQuery || global.Zepto;
+    return eventProvider && eventProvider({}) || fallback;
+  })();
+
+  //
+  //
+  //
+  AutoUpdate.isSupported = function isSupported() {
+    var hasAppCacheSupport = !! global.applicationCache;
+    var documentHasManifestAttribute = !! document.documentElement.getAttribute('manifest');
+
+    return hasAppCacheSupport && documentHasManifestAttribute;
+  };
+
+  //
+  //
+  //
+  var doneSetup = false;
+  var noop = function(){};
+  AutoUpdate.setup = function setup() {
+    if (doneSetup) return;
+    doneSetup = true;
+
+    if (! AutoUpdate.isSupported()) {
+      AutoUpdate.check = noop;
+      return;
+    }
+
+    // Fired when the manifest resources have been newly redownloaded.
+    on('updateready', function() {
+      // I have seen both Chorme & Firefox throw exceptions when trying
+      // to swap cache on updateready. I was not able to reproduce it,
+      // for for the sake of sanity, I'm making it fail silently
+      try {
+        AutoUpdate.swapCache();
+        hasUpdateFlag = true;
+        AutoUpdate.trigger('updateready');
+      } catch(error) {}
+    });
+
+    // Fired after the first cache of the manifest.
+    on('cached', function() {
+      hasUpdateFlag = true;
+    });
+
+    // fired when manifest download failed
+    on('error',        handleNetworkError);
+
+    // fired when manifest download succeeded
+    on('noupdate',     handleNetworkSucces);
+    on('cached',       handleNetworkSucces);
+    on('updateready',  handleNetworkSucces);
+    on('progress',     handleNetworkSucces);
+    on('downloading',  handleNetworkSucces);
+
+    // auto update check
+    AutoUpdate.start();
+
+    // when browser goes online/offline, trigger check to double check.
+    global.addEventListener('online', AutoUpdate.check, false);
+    global.addEventListener('offline', AutoUpdate.check, false);
+  };
+
+  //
+  // request the appcache.manifest file and check if there's an update
+  //
+  AutoUpdate.check = function check() {
+    applicationCache.check();
+  };
+
+  //
+  // start auto updating. Optionally pass interval in ms to
+  // overwrite the current.
+  //
+  var intervalPointer;
+  AutoUpdate.start = function start (interval) {
+    if (interval) checkInterval = interval;
+
+    global.clearInterval(intervalPointer);
+    intervalPointer = global.setInterval(AutoUpdate.check, checkInterval);
+  };
+
+  //
+  // stop auto updating
+  //
+  AutoUpdate.stop = function stop() {
+    global.clearInterval(intervalPointer);
+  };
+
+  //
+  // returns true if an update has been fully received, otherwise false
+  //
+  AutoUpdate.hasUpdate = function hasUpdate() {
+    return hasUpdateFlag;
+  };
+
+  //
+  // overwrite default checkInterval
+  // Do not allow less than 1s
+  //
+  AutoUpdate.setInterval = function setInterval(intervalInMs) {
+    checkInterval = Math.max(parseInt(intervalInMs) || 0, 1000);
+  };
+
+  // Private
+  // -------
+
+  // default check interval in ms
+  var checkInterval = 30000;
+
+  // optional: shorter interval when offline
+  var checkOfflineInterval = 3000;
+
+  // flag if there is a pending update, being applied after next page reload
+  var hasUpdateFlag = false;
+
+  // flag if there was an error updating the appCache, usually meaning
+  // it couldn't connect, a.k.a. you're offline.
+  var hasNetworkError = false;
+
+
+  //
+  // interface to bind events to cache events
+  //
+  function on(event, callback) {
+    applicationCache.addEventListener(event, callback, false);
+  }
+
+  //
+  //
+  //
+  function handleNetworkSucces() {
+    if (! hasNetworkError) return;
+    hasNetworkError = false;
+
+    // reset check interval
+    AutoUpdate.start(checkInterval);
+
+    AutoUpdate.trigger('online');
+  }
+
+  //
+  //
+  //
+  function handleNetworkError () {
+    if (hasNetworkError) return;
+    hasNetworkError = true;
+
+    // Edge case: private mode in Safari & FF say they support applicationCache,
+    // but they fail. To get arround that, we only trigger the offline event
+    // when applicationCache.status != uncached
+    if (applicationCache.status === applicationCache.UNCACHED) return;
+
+    // check with offline interval
+    AutoUpdate.start(checkOfflineInterval || checkInterval);
+
+    AutoUpdate.trigger('offline');
+  }
+})(window);
