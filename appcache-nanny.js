10a5bd485d0ba034c6d1f9e84e3b6d3d67fb6cae
cdaringe <cdieringer@mrn.org>
2014-08-28 21:26:19 -0700

    updated id of iframe

--- appcache-nanny.js
+++ appcache-nanny.js
@@ -230,7 +230,7 @@
     // load the appcache-loader.html using an iframe
     iframe = document.createElement('iframe');
     iframe.src = nannyOptions.loaderPath;
-    iframe.id = 'appCacheNanny';
+    iframe.id = 'appCacheNannyLoader';
     iframe.style.display = 'none';
     iframe.onload = function() {
       // we use the iFrame's applicationCache Object now
