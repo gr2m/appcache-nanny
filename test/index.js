e778c9296372c9ee44c41b75113b2062b6a7fd2e
Renie Siqueira <renie.siqueira@softexpert.com>
2015-06-17 15:32:58 -0300

    test: pass applicationCache event to appCache event handlers

--- test/index.js
+++ test/index.js
@@ -15,12 +15,21 @@ module.exports = function(browser, options, callback) {
     // cache button should be visible, as app is not cached initially
     .elementByCss('#btn-cache').isDisplayed()
       .should.become(true)
+    // as it isn't cached yet, progress report option should be visible  
+    .elementByCss('#chk-progress').isDisplayed()
+      .should.become(true)
+    // check to enable progress report
+    .elementByCss('#chk-progress').click()
     // when clicked on cache button, "cached" event should eventually appear
     .elementByCss('#btn-cache').click()
 
     .waitForConditionInBrowser('((document.querySelector("#logs") && document.querySelector("#logs").textContent) || "").indexOf("cached") >= 0', 10 * 1000, 1000)
     .elementByCss('#logs').text()
       .should.eventually.match(/cached/)
+
+    // check if progress report is there
+    .elementByCss('#logs').text()
+      .should.eventually.match(/Already loaded [0-9]{0,3} from [0-9]{0,3} \([0-9]{0,3}\%\)/)
 
     // non-existing paths should no load due to the appCache FALLBACK: / /
     .get(options.baseUrl + '/appcache-fallback-test')
@@ -31,6 +40,10 @@ module.exports = function(browser, options, callback) {
     // Is cached? should become "yes"
     .waitForConditionInBrowser('document.body.dataset.iscached === "1"', 10 * 1000)
 
+    // as now we have cache, progress report option should NOT be visible  
+    .elementByCss('#chk-progress').isDisplayed()
+      .should.become(false)
+      
     // check for update
     .elementByCss('#btn-check').click()
 
